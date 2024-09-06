af8c2fa2b0b435a1bdd952d8f696fa55353c50a7
Duc Linh <linhtd.caet@gmail.com>
2024-09-06 17:03:21 +0700

    aa

---
+++ VoteDialog/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VoteDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Item> Items { get; set; }
+        public class Item
+        {
+            public string Text { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+ 
+
+            Items = new ObservableCollection<Item>
+            {
+                new Item { Text = "Lựa chọn 1" },
+                new Item { Text = "Lựa chọn 2" },
+            };
+
+            // Gán DataContext cho cửa sổ
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //// Kiểm tra xem cột 2 có đang hiển thị không
+            //if (OptionColumn.Width.Value > 0)
+            //{
+            //    // Ẩn cột 2 bằng cách đặt Width là 0
+            //    OptionColumn.Width = new GridLength(0);
+            //}
+            //else
+            //{
+            //    // Hiển thị lại cột 2 bằng cách đặt Width là *
+            //    OptionColumn.Width = new GridLength(1, GridUnitType.Star);
+            //}
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Items.Add(new Item { Text = $"Lựa chọn {Items.Count + 1}" });
+            this.SizeToContent = SizeToContent.Height;
+        }
+    }
+}
---
+++ VoteDialog/MyCustomTextBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace VoteDialog;
+
+public class MyCustomTextBox : TextBox
+{
+    static MyCustomTextBox()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCustomTextBox), new FrameworkPropertyMetadata(typeof(MyCustomTextBox)));
+    }
+
+    // Bạn có thể thêm các thuộc tính hoặc phương thức tùy chỉnh tại đây
+}
